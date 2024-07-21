caf4791fa4d5445eb2e00e8e71b3945bc30c3267
Meshal <mesh3l-966@hotmail.com>
2024-07-21 10:23:08 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,24 +3,24 @@ def add(x, y):
 
 
 def subtract(x, y):
-    pass
+    return x - y
 
 
 def divide(x, y):
-    pass
+    x / y
 
 
 def multiply(x, y):
-    pass
+    x * y
 
 
 def square(x):
-    pass
+    x ** 2
 
 
 def power(x, y):
-    pass
+    x ** y
 
 
 def sqrt(x):
-    pass
+    x ** 0.5
